3de92b8ded8e7ddcf362d8a4f8ca073859d0bb9d
NoNameA 774 <nonamea.774@gmail.com>
2015-10-17 03:58:08 +0900

    add div by 2 test

---
+++ test/div_by_2.hpp
@@ -0,0 +1,10 @@
+#include<color.hpp>
+
+Color piet[] = { 
+  LRed, DBlue, DGreen, LYellow, LMagenta, DYellow, Cyan, DRed, Black, White, DRed,
+  White, White, White, White, White, White, White, DRed, DRed, White, DRed,
+  White, DCyan, White, White, White, Black, White, Yellow, Black, White, DRed,
+  White, DCyan, DCyan, DCyan, DCyan, DCyan, DYellow, Yellow, White, Black, DRed,
+ };
+size_t width = 11;
+size_t height = 4;
